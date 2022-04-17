55b3ba5526844f48fb37f6f171323fa3c5ac0feb
Abián Izquierdo <abizmo@gmail.com>
2022-04-17 19:54:40 +0100

    Set global css

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <main>
+    <main className="container">
       Social Media Dashboard
       Total Followers: 23,004
 
