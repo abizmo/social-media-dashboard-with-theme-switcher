86fd97847fd553457a004d286f389391d8eebcef
Abián Izquierdo <abizmo@gmail.com>
2022-04-18 10:40:30 +0100

    Create Dashboard component

--- src/components/Dashboard/Header/index.jsx
+++ src/components/Dashboard/Header/index.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 
+import ThemeContext from '../../../ThemeContext';
 import styles from './Header.module.css';
 
 function Header() {
-  const [dark, setDark] = useState(true);
-
-  const handleSwitchMode = () => setDark((prev) => !prev);
+  const { dark, handleSwitchMode } = useContext(ThemeContext);
 
   return (
-    <div className={`flex ${styles.header}`}>
+    <div
+      className={`flex ${styles.header}`}
+    >
       <div className={`flex ${styles.info}`}>
         <h1 className="fs-700">Social Media Dashboard</h1>
         <p className="letter-spacing-3 text-secondary">Total Followers: 23,004</p>
